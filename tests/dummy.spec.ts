ecfe6eed2726990aeb7c18655f0f8343e2d55274
“talatkuyuk” <talatkuyuk@gmail.com>
2023-01-20 00:28:22 +0300

    added test files

--- tests/dummy.spec.ts
+++
@@ -1,8 +0,0 @@
-describe("dummy test", () => {
-  it("Line numbers text input should match with the regex", () => {
-    const RE = /{([\d,-]+)}/g;
-    const input = "{1,2-4}";
-    const match = input.match(RE);
-    expect(match).not.toBeNull();
-  });
-});
