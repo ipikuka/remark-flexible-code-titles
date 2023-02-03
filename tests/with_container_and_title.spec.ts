8a300bf41ffa32b892df33d9032f6fca1f07da54
talatkuyuk <talatkuyuk@gmail.com>
2023-02-03 20:49:03 +0300

    removed remark; added unified and remark-parse in test environment

--- tests/with_container_and_title.spec.ts
+++ tests/with_container_and_title.spec.ts
@@ -1,180 +1,200 @@
-import type { Processor } from "unified";
-import type { VFileCompatible } from "vfile";
-import { remark } from "remark";
+import { unified, type Processor } from "unified";
+import remarkParse from "remark-parse";
 import gfm from "remark-gfm";
-import remark2rehype from "remark-rehype";
-import stringify from "rehype-stringify";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
 import dedent from "dedent";
+import type { VFileCompatible } from "vfile";
 
 import plugin from "../src/index";
 
-const compiler: Processor = remark()
+const compiler: Processor = unified()
+  .use(remarkParse)
   .use(gfm)
   .use(plugin)
-  .use(remark2rehype)
-  .use(stringify);
+  .use(remarkRehype)
+  .use(rehypeStringify);
 
 const process = async (contents: VFileCompatible): Promise<VFileCompatible> => {
   return compiler.process(contents).then((file) => file.value);
 };
 
 describe("remark-flexible-code-title, no options, meaningly with container and title by default", () => {
-  /** */
+  // ******************************************
   it("Considers there is no language or title", async () => {
     const input = dedent`
-\`\`\`
-const a = 1;
-\`\`\`
-`;
+      \`\`\`
+      const a = 1;
+      \`\`\`
+    `;
 
     // { type: 'code', lang: null, meta: null, parent: 'root' }
 
-    const expected = `<div class="remark-code-container"><pre><code>const a = 1;
-</code></pre></div>`;
+    const expected = dedent`
+      <div class="remark-code-container"><pre><code>const a = 1;
+      </code></pre></div>
+    `;
 
     expect(await process(input)).toBe(expected);
   });
 
-  /** */
+  // ******************************************
   it("Considers there is no language or title, but only colon", async () => {
     const input = dedent`
-\`\`\`:
-const a = 1;
-\`\`\`
-`;
+      \`\`\`:
+      const a = 1;
+      \`\`\`
+    `;
 
     // { type: 'code', lang: ':', meta: null, parent: 'root' }  ==>> make the lang null
 
-    const expected = `<div class="remark-code-container"><pre><code>const a = 1;
-</code></pre></div>`;
+    const expected = dedent`
+      <div class="remark-code-container"><pre><code>const a = 1;
+      </code></pre></div>
+    `;
 
     expect(await process(input)).toBe(expected);
   });
 
-  /** */
+  // ******************************************
   it("Considers there is only the regular language", async () => {
     const input = dedent`
-\`\`\`js
-const a = 1;
-\`\`\`
-`;
+      \`\`\`js
+      const a = 1;
+      \`\`\`
+    `;
 
     // { type: 'code', lang: 'js', meta: null, parent: 'root' }
 
-    const expected = `<div class="remark-code-container"><pre><code class="language-js">const a = 1;
-</code></pre></div>`;
+    const expected = dedent`
+      <div class="remark-code-container"><pre><code class="language-js">const a = 1;
+      </code></pre></div>
+    `;
 
     expect(await process(input)).toBe(expected);
   });
 
-  /** */
+  // ******************************************
   it("Handles there is no title but colon", async () => {
     const input = dedent`
-\`\`\`js:
-const a = 1;
-\`\`\`
-`;
+      \`\`\`js:
+      const a = 1;
+      \`\`\`
+    `;
 
     // { type: 'code', lang: 'js:', meta: null, parent: 'root' } ==>> delete the colon in the lang
 
-    const expected = `<div class="remark-code-container"><pre><code class="language-js">const a = 1;
-</code></pre></div>`;
+    const expected = dedent`
+      <div class="remark-code-container"><pre><code class="language-js">const a = 1;
+      </code></pre></div>
+    `;
 
     expect(await process(input)).toBe(expected);
   });
 
-  /** */
+  // ******************************************
   it("Handles there is no language but colon", async () => {
     const input = dedent`
-\`\`\`:title.js
-const a = 1;
-\`\`\`
-`;
+      \`\`\`:title.js
+      const a = 1;
+      \`\`\`
+    `;
 
     // ==>> make the lang null
     // { type: 'code', lang: ':title.js', meta: null, parent: 'root' } first visit
     // { type: 'code', lang: '', meta: null, parent: 'root' } second visit ==>> see the lang: null
 
-    const expected = `<div class="remark-code-container"><div class="remark-code-title">title.js</div><pre><code>const a = 1;
-</code></pre></div>`;
+    const expected = dedent`
+      <div class="remark-code-container"><div class="remark-code-title">title.js</div><pre><code>const a = 1;
+      </code></pre></div>
+    `;
 
     expect(await process(input)).toBe(expected);
   });
 
-  /** */
+  // ******************************************
   it("Handles there is syntax mismatch, having one space around the colon", async () => {
     const input = dedent`
-\`\`\`js :    title.js   meta
-const a = 1;
-\`\`\`
-`;
+      \`\`\`js :    title.js   meta
+      const a = 1;
+      \`\`\`
+    `;
+
     // little complex logic; meta starts with the colon, so remove the first word from meta, if this is not reserved
     // { type: 'code', lang: 'js', meta: ':    title.js   meta', parent: 'root' } ==>> little complex logic; remove title from meta
 
-    const expected = `<div class="remark-code-container"><div class="remark-code-title">title.js</div><pre><code class="language-js">const a = 1;
-</code></pre></div>`;
+    const expected = dedent`
+      <div class="remark-code-container"><div class="remark-code-title">title.js</div><pre><code class="language-js">const a = 1;
+      </code></pre></div>
+    `;
 
     expect(await process(input)).toBe(expected);
   });
 
-  /** */
+  // ******************************************
   it("Handles there is syntax mismatch, having more space at the right of the colon", async () => {
     const input = dedent`
-\`\`\`js:    title.js    meta
-const a = 1;
-\`\`\`
-`;
+      \`\`\`js:    title.js    meta
+      const a = 1;
+      \`\`\`
+    `;
 
     // ==>> nothing to do, there is no title, but remove the colon from the language at the end
     // { type: 'code', lang: 'js:', meta: 'title.js    meta', parent: 'root' }
 
-    const expected = `<div class="remark-code-container"><div class="remark-code-title">title.js</div><pre><code class="language-js">const a = 1;
-</code></pre></div>`;
+    const expected = dedent`
+      <div class="remark-code-container"><div class="remark-code-title">title.js</div><pre><code class="language-js">const a = 1;
+      </code></pre></div>
+    `;
 
     expect(await process(input)).toBe(expected);
   });
 
-  /** */
+  // ******************************************
   it("Handles there is syntax mismatch, having more space at the both side of the colon", async () => {
     const input = dedent`
-\`\`\`js    :      title.js    meta
-const a = 1;
-\`\`\`
-`;
+      \`\`\`js    :      title.js    meta
+      const a = 1;
+      \`\`\`
+    `;
 
     // little complex logic; meta starts with the colon, so remove the first word from meta, if this is not reserved
     // { type: 'code', lang: 'js', meta: ':      title.js    meta', parent: 'root' }
 
-    const expected = `<div class="remark-code-container"><div class="remark-code-title">title.js</div><pre><code class="language-js">const a = 1;
-</code></pre></div>`;
+    const expected = dedent`
+      <div class="remark-code-container"><div class="remark-code-title">title.js</div><pre><code class="language-js">const a = 1;
+      </code></pre></div>
+    `;
 
     expect(await process(input)).toBe(expected);
   });
 
-  /** */
+  // ******************************************
   it("Handles the regular language and the regular title", async () => {
     const input = dedent`
-\`\`\`js:title.js
-const a = 1;
-\`\`\`
-`;
+      \`\`\`js:title.js
+      const a = 1;
+      \`\`\`
+    `;
 
     // { type: 'code', lang: 'js:title.js', meta: null, parent: 'root' } first visit
     // { type: 'code', lang: 'js', meta: null, parent: 'root' } second visit
 
-    const expected = `<div class="remark-code-container"><div class="remark-code-title">title.js</div><pre><code class="language-js">const a = 1;
-</code></pre></div>`;
+    const expected = dedent`
+      <div class="remark-code-container"><div class="remark-code-title">title.js</div><pre><code class="language-js">const a = 1;
+      </code></pre></div>
+    `;
 
     expect(await process(input)).toBe(expected);
   });
 
-  /** */
+  // ******************************************
   it("Handles unknown language and the line range string within curly braces", async () => {
     const input = dedent`
-\`\`\`{2,    3   -   4,   5}         showLineNumbers  
-const a = 1;
-\`\`\`
-`;
+      \`\`\`{2,    3   -   4,   5}         showLineNumbers  
+      const a = 1;
+      \`\`\`
+    `;
 
     /*
       {
@@ -187,19 +207,21 @@ const a = 1;
 
     // { title: undefined, language: null, meta: '{2,3-4,5} showLineNumbers' }
 
-    const expected = `<div class="remark-code-container"><pre><code>const a = 1;
-</code></pre></div>`;
+    const expected = dedent`
+      <div class="remark-code-container"><pre><code>const a = 1;
+      </code></pre></div>
+    `;
 
     expect(await process(input)).toBe(expected);
   });
 
-  /** */
+  // ******************************************
   it("Handles the line range string within curly braces with language", async () => {
     const input = dedent`
-\`\`\`ts{2  , 3   -   4,   5}         showLineNumbers  
-const a = 1;
-\`\`\`
-`;
+      \`\`\`ts{2  , 3   -   4,   5}         showLineNumbers  
+      const a = 1;
+      \`\`\`
+    `;
 
     /*
       {
@@ -212,19 +234,21 @@ const a = 1;
 
     // { title: undefined, language: 'ts', meta: '{2,3-4,5} showLineNumbers' }
 
-    const expected = `<div class="remark-code-container"><pre><code class="language-ts">const a = 1;
-</code></pre></div>`;
+    const expected = dedent`
+      <div class="remark-code-container"><pre><code class="language-ts">const a = 1;
+      </code></pre></div>
+    `;
 
     expect(await process(input)).toBe(expected);
   });
 
-  /** */
+  // ******************************************
   it("Handles the line range string within curly braces with language and title", async () => {
     const input = dedent`
-\`\`\`typescript:title{2  , 3   -   4,   5}         showLineNumbers  
-const a = 1;
-\`\`\`
-`;
+      \`\`\`typescript:title{2  , 3   -   4,   5}         showLineNumbers  
+      const a = 1;
+      \`\`\`
+    `;
 
     /*
       {
@@ -237,8 +261,10 @@ const a = 1;
 
     // { title: undefined, language: 'typescript', meta: '{2,3-4,5} showLineNumbers' }
 
-    const expected = `<div class="remark-code-container"><div class="remark-code-title">title</div><pre><code class="language-typescript">const a = 1;
-</code></pre></div>`;
+    const expected = dedent`
+      <div class="remark-code-container"><div class="remark-code-title">title</div><pre><code class="language-typescript">const a = 1;
+      </code></pre></div>
+    `;
 
     expect(await process(input)).toBe(expected);
   });
