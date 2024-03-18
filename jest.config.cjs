810bee72fc346586509112c2b757e7d6690df84f
“talatkuyuk” <talatkuyuk@gmail.com>
2024-03-18 06:09:56 +0300

    added test coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,4 +3,14 @@ module.exports = {
   preset: "ts-jest/presets/default-esm-legacy",
   testEnvironment: "node",
   prettierPath: require.resolve("prettier-2"),
+  collectCoverageFrom: ["src/index.ts"],
+  coverageReporters: ["json", "lcov", "text"],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 };
