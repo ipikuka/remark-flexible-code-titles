58104feeaf4efdee737c0fa077ab36a1ee386ba2
“talatkuyuk” <talatkuyuk@gmail.com>
2024-03-20 21:54:21 +0300

    added a fixture test and updated all tests

---
+++ tests/test.fixture.spec.ts
@@ -0,0 +1,584 @@
+import dedent from "dedent";
+
+import { processMDAST } from "./util/index";
+
+const fixture = dedent`
+  :
+
+   :
+    :
+  js
+   js
+  js:
+   js:
+  js :
+   js :
+  js:title
+  js :title
+  js: title
+  js : title
+  js{1,2}
+  js {1,2}
+  js { 1, 2  }
+  js{ 1, 2}
+  js{1 ,2}
+  js{1 , 2 }
+  js:title{1,2}
+  js:title{ 1,2}
+  js:title{1, 2  }
+  js:title {1,2}
+  js:title {1, 2  }
+  js:title { 1, 2  }
+  js :title{1,2}
+  js :title{1 ,2 }
+  js :title {1,2}
+  js :title { 1 , 2 }
+  js: title{1,2}
+  js: title{1 ,2 }
+  js: title {1,2}
+  js: title { 1 , 2 }
+  js : title{1,2}
+  js : title{1 ,2 }
+  js : title {1,2}
+  js : title { 1 , 2 }
+  js:title{1,2}showLineNumbers
+  js:title{1,2} showLineNumbers
+  js:title {1,2} showLineNumbers
+  js:title { 1, 2} showLineNumbers
+  js:title {1, 2}showLineNumbers
+  js:{1,2}showLineNumbers
+  js:{1,2} showLineNumbers
+  js: {1,2} showLineNumbers
+  js: { 1, 2} showLineNumbers
+  js: {1, 2}showLineNumbers
+  js :{1,2}showLineNumbers
+  js :{1,2} showLineNumbers
+  js : {1,2} showLineNumbers
+  js : { 1, 2} showLineNumbers
+  js : {1, 2}showLineNumbers
+  js{1,2}showLineNumbers
+  js{1,2} showLineNumbers
+  js {1,2} showLineNumbers
+  js { 1, 2} showLineNumbers
+  js {1, 2}showLineNumbers
+  js:showLineNumbers{1,2}
+  js: showLineNumbers{1,2}
+  js :showLineNumbers{1,2}
+  js : showLineNumbers{1,2}
+  js showLineNumbers {1,2}
+  js showLineNumbers {1 , 2 }
+  js:title showLineNumbers {1 , 2 }
+  js :title showLineNumbers {1 , 2 }
+  js : title showLineNumbers {1 , 2 }
+  js :showLineNumbers {1 , 2 }
+  js : showLineNumbers {1 , 2 }
+  showLineNumbers{1,2}
+  showLineNumbers {1 , 2 }
+  {1,2}showLineNumbers
+  {1 , 2}showLineNumbers
+  showLineNumbers{1,2}:title
+  showLineNumbers{1,2} : title
+  showLineNumbers {1 , 2 }:title
+  showLineNumbers {1 , 2 } :title
+  {1,2}showLineNumbers:title
+  {1,2}showLineNumbers : title
+  {1 , 2}showLineNumbers:title
+  {1, 2}showLineNumbers :  title
+`;
+
+it("parses the language, title and meta correctly", async () => {
+  const result = fixture
+    .split("\n")
+    .map((input) => ({ _____: input, ...processMDAST("```" + input + "\n```") }));
+
+  expect(result).toMatchInlineSnapshot(`
+    [
+      {
+        "_____": ":",
+        "_lang": null,
+        "_meta": null,
+        "title": null,
+      },
+      {
+        "_____": "",
+        "_lang": null,
+        "_meta": null,
+        "title": null,
+      },
+      {
+        "_____": " :",
+        "_lang": null,
+        "_meta": null,
+        "title": null,
+      },
+      {
+        "_____": "  :",
+        "_lang": null,
+        "_meta": null,
+        "title": null,
+      },
+      {
+        "_____": "js",
+        "_lang": "js",
+        "_meta": null,
+        "title": null,
+      },
+      {
+        "_____": " js",
+        "_lang": "js",
+        "_meta": null,
+        "title": null,
+      },
+      {
+        "_____": "js:",
+        "_lang": "js",
+        "_meta": null,
+        "title": null,
+      },
+      {
+        "_____": " js:",
+        "_lang": "js",
+        "_meta": null,
+        "title": null,
+      },
+      {
+        "_____": "js :",
+        "_lang": "js",
+        "_meta": null,
+        "title": null,
+      },
+      {
+        "_____": " js :",
+        "_lang": "js",
+        "_meta": null,
+        "title": null,
+      },
+      {
+        "_____": "js:title",
+        "_lang": "js",
+        "_meta": null,
+        "title": "title",
+      },
+      {
+        "_____": "js :title",
+        "_lang": "js",
+        "_meta": null,
+        "title": "title",
+      },
+      {
+        "_____": "js: title",
+        "_lang": "js",
+        "_meta": null,
+        "title": "title",
+      },
+      {
+        "_____": "js : title",
+        "_lang": "js",
+        "_meta": null,
+        "title": "title",
+      },
+      {
+        "_____": "js{1,2}",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": null,
+      },
+      {
+        "_____": "js {1,2}",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": null,
+      },
+      {
+        "_____": "js { 1, 2  }",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": null,
+      },
+      {
+        "_____": "js{ 1, 2}",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": null,
+      },
+      {
+        "_____": "js{1 ,2}",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": null,
+      },
+      {
+        "_____": "js{1 , 2 }",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": null,
+      },
+      {
+        "_____": "js:title{1,2}",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js:title{ 1,2}",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js:title{1, 2  }",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js:title {1,2}",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js:title {1, 2  }",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js:title { 1, 2  }",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js :title{1,2}",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js :title{1 ,2 }",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js :title {1,2}",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js :title { 1 , 2 }",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js: title{1,2}",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js: title{1 ,2 }",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js: title {1,2}",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js: title { 1 , 2 }",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js : title{1,2}",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js : title{1 ,2 }",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js : title {1,2}",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js : title { 1 , 2 }",
+        "_lang": "js",
+        "_meta": "{1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js:title{1,2}showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": "title",
+      },
+      {
+        "_____": "js:title{1,2} showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": "title",
+      },
+      {
+        "_____": "js:title {1,2} showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": "title",
+      },
+      {
+        "_____": "js:title { 1, 2} showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": "title",
+      },
+      {
+        "_____": "js:title {1, 2}showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": "title",
+      },
+      {
+        "_____": "js:{1,2}showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "js:{1,2} showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "js: {1,2} showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "js: { 1, 2} showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "js: {1, 2}showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "js :{1,2}showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "js :{1,2} showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "js : {1,2} showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "js : { 1, 2} showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "js : {1, 2}showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "js{1,2}showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "js{1,2} showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "js {1,2} showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "js { 1, 2} showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "js {1, 2}showLineNumbers",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "js:showLineNumbers{1,2}",
+        "_lang": "js",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "js: showLineNumbers{1,2}",
+        "_lang": "js",
+        "_meta": "showLineNumbers {1,2}",
+        "title": null,
+      },
+      {
+        "_____": "js :showLineNumbers{1,2}",
+        "_lang": "js",
+        "_meta": "showLineNumbers {1,2}",
+        "title": null,
+      },
+      {
+        "_____": "js : showLineNumbers{1,2}",
+        "_lang": "js",
+        "_meta": "showLineNumbers {1,2}",
+        "title": null,
+      },
+      {
+        "_____": "js showLineNumbers {1,2}",
+        "_lang": "js",
+        "_meta": "showLineNumbers {1,2}",
+        "title": null,
+      },
+      {
+        "_____": "js showLineNumbers {1 , 2 }",
+        "_lang": "js",
+        "_meta": "showLineNumbers {1,2}",
+        "title": null,
+      },
+      {
+        "_____": "js:title showLineNumbers {1 , 2 }",
+        "_lang": "js",
+        "_meta": "showLineNumbers {1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js :title showLineNumbers {1 , 2 }",
+        "_lang": "js",
+        "_meta": "showLineNumbers {1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js : title showLineNumbers {1 , 2 }",
+        "_lang": "js",
+        "_meta": "showLineNumbers {1,2}",
+        "title": "title",
+      },
+      {
+        "_____": "js :showLineNumbers {1 , 2 }",
+        "_lang": "js",
+        "_meta": "showLineNumbers {1,2}",
+        "title": null,
+      },
+      {
+        "_____": "js : showLineNumbers {1 , 2 }",
+        "_lang": "js",
+        "_meta": "showLineNumbers {1,2}",
+        "title": null,
+      },
+      {
+        "_____": "showLineNumbers{1,2}",
+        "_lang": null,
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "showLineNumbers {1 , 2 }",
+        "_lang": "",
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "{1,2}showLineNumbers",
+        "_lang": null,
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "{1 , 2}showLineNumbers",
+        "_lang": null,
+        "_meta": "{1,2} showLineNumbers",
+        "title": null,
+      },
+      {
+        "_____": "showLineNumbers{1,2}:title",
+        "_lang": null,
+        "_meta": "{1,2} showLineNumbers",
+        "title": "title",
+      },
+      {
+        "_____": "showLineNumbers{1,2} : title",
+        "_lang": null,
+        "_meta": "{1,2} showLineNumbers",
+        "title": "title",
+      },
+      {
+        "_____": "showLineNumbers {1 , 2 }:title",
+        "_lang": "",
+        "_meta": "{1,2} showLineNumbers",
+        "title": "title",
+      },
+      {
+        "_____": "showLineNumbers {1 , 2 } :title",
+        "_lang": "",
+        "_meta": "{1,2} showLineNumbers",
+        "title": "title",
+      },
+      {
+        "_____": "{1,2}showLineNumbers:title",
+        "_lang": null,
+        "_meta": "{1,2} showLineNumbers",
+        "title": "title",
+      },
+      {
+        "_____": "{1,2}showLineNumbers : title",
+        "_lang": null,
+        "_meta": "{1,2} showLineNumbers",
+        "title": "title",
+      },
+      {
+        "_____": "{1 , 2}showLineNumbers:title",
+        "_lang": null,
+        "_meta": "{1,2} showLineNumbers",
+        "title": "title",
+      },
+      {
+        "_____": "{1, 2}showLineNumbers :  title",
+        "_lang": null,
+        "_meta": "{1,2} showLineNumbers",
+        "title": "title",
+      },
+    ]
+  `);
+});
--- tests/util/index.ts
+++ tests/util/index.ts
@@ -5,6 +5,11 @@ import remarkRehype from "remark-rehype";
 import rehypeFormat from "rehype-format";
 import rehypeStringify from "rehype-stringify";
 import type { VFileCompatible, Value } from "vfile";
+import type { Paragraph, Code, Text } from "mdast";
+import { fromMarkdown } from "mdast-util-from-markdown";
+import { gfm as gfmExt } from "micromark-extension-gfm";
+import { gfmFromMarkdown } from "mdast-util-gfm";
+import { find } from "unist-util-find";
 
 import plugin, { CodeTitleOptions } from "../../src";
 
@@ -24,4 +29,44 @@ export const process = async (
   const vFile = await compilerCreator(options).process(content);
 
   return vFile.value;
+};
+
+/**
+ * finds the AST node (Code) and the title node after processed via plugin
+ * @param content string ;
+ * @returns AST node (Code)
+ */
+export const processMDAST = (
+  content: string,
+  options?: CodeTitleOptions,
+):
+  | {
+      title: string | null;
+      lang: string | null | undefined;
+      meta: string | null | undefined;
+    }
+  | undefined => {
+  const tree = fromMarkdown(content, {
+    extensions: [gfmExt()],
+    mdastExtensions: [gfmFromMarkdown()],
+  });
+
+  const code = find<Code>(tree, { type: "code" });
+
+  if (!code) return;
+
+  // @ts-expect-error
+  plugin(options)(tree);
+
+  const code_ = find<Code>(tree, { type: "code" });
+
+  if (!code_) return;
+
+  const lang = code_.lang;
+  const meta = code_.meta;
+
+  const titleNode = find<Paragraph>(tree, { type: "paragraph" })?.children[0];
+  const title = titleNode ? (titleNode as Text).value : null;
+
+  return { title, lang, meta };
 };
